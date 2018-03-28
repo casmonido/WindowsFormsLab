457e83c6a04e92c7130d2cae1736d9b5099f6e66
casmonido <K.Komar@stud.elka.pw.edu.pl>
2018-03-28 10:06:29 +0200

    prevents closing last form

--- WindowsFormsApp/MainForm.cs
+++ WindowsFormsApp/MainForm.cs
@@ -20,6 +20,7 @@ namespace WindowsFormsApp
             InitializeComponent();
             LayoutMdi(MdiLayout.Cascade);
             this.MdiChildActivate += MdiChildActiveChanged;
+            this.FormClosing += ThisFrame_FormClosing;
         }
 
         private void MdiChildActiveChanged(object sender, EventArgs e)
@@ -41,7 +42,26 @@ namespace WindowsFormsApp
             Lista lista = new Lista(ref listaFigur);
             widoki.Add(lista);
             lista.MdiParent = this;
+            lista.FormClosing += Frame_FormClosing;
             lista.Show();
+        }
+
+        private void ThisFrame_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            widoki.Clear();
+            e.Cancel = false;
+            return;
+        }
+
+
+        private void Frame_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (widoki.Count == 1)
+            {
+                e.Cancel = true;
+                return;
+            }
+            widoki.Remove((RefreshableForm)sender);
         }
 
         private void mDIToolStripMenuItem_Click(object sender, EventArgs e)
